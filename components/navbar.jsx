adc0d03e538ec2311d547ff5e8351a1d8bcaccd2
xitsMichIU <151556531+xitsMichIU@users.noreply.github.com>
2024-02-06 09:52:16 +0100

    Zwischenspeichern

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-
-
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: "#e3f2fd" }}>
